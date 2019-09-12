9005cdaad3e41560fc548ee37d18e7e2681989d6
zhaomanzhou <zmzsstreet@163.com>
2019-09-12 22:48:53 +0800

    The segment tree is fully completed

--- src/segment_tree/SegmentTree.java
+++ src/segment_tree/SegmentTree.java
@@ -44,6 +44,87 @@ public class SegmentTree<E>
     }
 
 
+    /**
+     * 返回区间[L, R]的值
+     * @param queryL
+     * @param queryR
+     * @return
+     */
+    public E query(int queryL, int queryR)
+    {
+        assert queryL >= 0 && queryL < data.length;
+        assert queryR >= 0 && queryR < data.length;
+        return query(0, 0, data.length-1, queryL, queryR);
+    }
+
+    /**
+     * 在以trteeID为根的线段树中[l...r]的范围内，搜索区间[queryL...queryR]的值
+     * @return
+     */
+    private E query(int treeIndex, int l, int r, int queryL, int queryR)
+    {
+        if(l == queryL && r == queryR)
+            return tree[treeIndex];
+        int mid = l + (r - l)/2;
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        //查询区间在右部
+        if(queryL >= mid + 1)
+        {
+            return query(rightTreeIndex, mid+1, r, queryL, queryR);
+        }
+
+        //查询区间在左部
+        else if(queryR <= mid)
+        {
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+        }
+
+        //左边右边都有
+        E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+        E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+
+        return operator.operate(leftResult, rightResult);
+
+    }
+
+
+    public void update(int index, E val)
+    {
+        assert index >= 0 && index < data.length;
+        set(0, 0, data.length-1, index, val);
+    }
+
+
+    /**
+     * 在以treeIndex为根的线段树更新index值为val
+     */
+    private void set(int treeIndex, int l, int r, int index, E val)
+    {
+        if(l == r)
+        {
+            tree[treeIndex] = val;
+            return;
+        }
+        int mid = l + (r-l)/2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        if(index >= mid+1)
+        {
+            set(treeIndex, mid+1, r, index, val);
+        }else
+        {
+            set(treeIndex, l, mid, index, val);
+        }
+
+        tree[treeIndex] = operator.operate(tree[leftTreeIndex], tree[rightTreeIndex]);
+
+    }
+
+
+
     public int size()
     {
         return data.length;
@@ -52,7 +133,7 @@ public class SegmentTree<E>
 
     public E get(int index)
     {
-        assert index > 0 || index < data.length;
+        assert index >= 0 && index < data.length;
         return data[index];
     }
 
