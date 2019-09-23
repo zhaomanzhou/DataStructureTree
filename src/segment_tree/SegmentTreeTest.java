e3ab2d830443d291eaff609d80636dcceb20ae86
zhaomanzhou <zmzsstreet@163.com>
2019-09-23 21:40:24 +0800

    Add binary search tree

--- src/segment_tree/SegmentTreeTest.java
+++ src/segment_tree/SegmentTreeTest.java
@@ -4,10 +4,13 @@ public class SegmentTreeTest
 {
     public static void main(String[] args)
     {
-        Integer[] arr = new Integer[]{1, 2, 3, 5};
+        Integer[] arr = new Integer[]{1, -4 ,2, 3, -7, 5};
         SegmentTree<Integer> tree = new SegmentTree<Integer>(arr, (a, b) -> a + b);
 
         System.out.println(tree);
+
+        System.out.println(tree.query(0, 1));
+        System.out.println(tree.query(2, 4));
 
     }
 }
